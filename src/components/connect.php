d158ad31021aa7414d0747c2350d47d281d0eef3
Soufiane-Ha <soufianehammadi02@gmail.com>
2025-05-05 21:24:00 +0100

    modification

--- src/components/connect.php
+++ src/components/connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('DB_HOST') ?: 'db';
-$db   = getenv('DB_NAME') ?: 'shop_db';
-$user = getenv('DB_USER') ?: 'myuser';
-$pass = getenv('DB_PASS') ?: 'mypass';
+$host = getenv('DB_HOST') ?: 'dpg-d0cfcj6uk2gs738mamd0-a';
+$db   = getenv('DB_NAME') ?: 'database_3qhp';
+$user = getenv('DB_USER') ?: 'database_3qhp_user';
+$pass = getenv('DB_PASS') ?: 'XDlQhcomymh3Ppq0Mpmx0ShfG1rVg9CF';
 $charset = 'utf8mb4';
 
 $dsn = "pgsql:host=$host;port=5432;dbname=$db";
